c3a7316fcf92c0d8c0d007d0143d5c3861eaaf41
masonwu21 <mason@emsoro.com>
2015-03-20 15:53:11 +0800

    原始版本

---
+++ ITVM3U8Monitor/ClassChannelName.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITVM3U8Monitor
+{
+    class ClassChannelName
+    {
+        //频道名称	channel_id
+
+//CCTV-1高清	134100
+//CCTV-3高清	134110
+//CCTV-4中文国际（亚）	101400
+//BTV-1高清	134160
+//DOX-映画	134340
+//DOX-剧场	134310
+//DOX-新知	134320
+//DOX-新艺	134330
+//东方卫视 高清	134230
+//江苏卫视 高清	134210
+//湖南卫视 高清	134200
+//浙江卫视 高清	134220
+//CCTV-5 高清	134350
+//江西卫视	112230
+//CCTV-6 高清	134130
+//深圳卫视	112400
+//CCTV-新闻	101490
+//广东卫视	112270
+//贵州卫视	112210
+//四川卫视	112320
+//劲爆体育	145730
+//安徽卫视	112240
+//CCTV-NEWS	101520
+//金鹰卡通	123320
+//CDTV-5	123742
+//SCTV-2	123732
+//CCTV5+	101710
+//国防军事	101620
+//SCTV-5	123735
+//CDTV-2	123739
+        public List<ChannelItem> chitem;
+        public ClassChannelName()
+        {
+            chitem = new List<ChannelItem>();
+            chitem.Add(new ChannelItem { id = "1", name = "CCTV-1高清", tvid = "134100" });
+            chitem.Add(new ChannelItem { id = "2", name = "CCTV-3高清", tvid = "134110" });
+            chitem.Add(new ChannelItem { id = "3", name = "CCTV-4中文国际", tvid = "101400" });
+            chitem.Add(new ChannelItem { id = "4", name = "BTV-1高清", tvid = "134160" });
+            chitem.Add(new ChannelItem { id = "5", name = "DOX-映画", tvid = "134340" });
+            chitem.Add(new ChannelItem { id = "6", name = "DOX-剧场", tvid = "134310" });
+            chitem.Add(new ChannelItem { id = "7", name = "DOX-新知", tvid = "134320" });
+            chitem.Add(new ChannelItem { id = "8", name = "DOX-新艺", tvid = "134330" });
+            chitem.Add(new ChannelItem { id = "9", name = "东方卫视 高清", tvid = "134230" });
+            chitem.Add(new ChannelItem { id = "10", name = "江苏卫视 高清", tvid = "134210" });
+            chitem.Add(new ChannelItem { id = "11", name = "湖南卫视 高清", tvid = "134200" });
+            chitem.Add(new ChannelItem { id = "12", name = "浙江卫视 高清", tvid = "134220" });
+            chitem.Add(new ChannelItem { id = "13", name = "CCTV-5 高清", tvid = "134350" });
+            chitem.Add(new ChannelItem { id = "14", name = "江西卫视", tvid = "112230" });
+            chitem.Add(new ChannelItem { id = "15", name = "CCTV-6 高清", tvid = "134130" });
+            chitem.Add(new ChannelItem { id = "16", name = "深圳卫视", tvid = "112400" });
+            chitem.Add(new ChannelItem { id = "17", name = "CCTV-新闻", tvid = "101490" });
+            chitem.Add(new ChannelItem { id = "18", name = "广东卫视", tvid = "112270" });
+            chitem.Add(new ChannelItem { id = "19", name = "贵州卫视", tvid = "112210" });
+            chitem.Add(new ChannelItem { id = "20", name = "四川卫视", tvid = "112320" });
+            chitem.Add(new ChannelItem { id = "21", name = "劲爆体育", tvid = "145730" });
+            chitem.Add(new ChannelItem { id = "22", name = "安徽卫视", tvid = "112240" });
+            chitem.Add(new ChannelItem { id = "23", name = "CCTV-NEWS", tvid = "101520" });
+            chitem.Add(new ChannelItem { id = "24", name = "金鹰卡通", tvid = "123320" });
+            chitem.Add(new ChannelItem { id = "25", name = "CDTV-5", tvid = "123742" });
+            chitem.Add(new ChannelItem { id = "26", name = "SCTV-2", tvid = "123732" });
+            chitem.Add(new ChannelItem { id = "27", name = "CCTV5+", tvid = "101710" });
+            chitem.Add(new ChannelItem { id = "28", name = "国防军事", tvid = "101620" });
+            chitem.Add(new ChannelItem { id = "29", name = "SCTV-5", tvid = "123735" });
+            chitem.Add(new ChannelItem { id = "30", name = "CDTV-2", tvid = "123739" });
+
+
+        }
+
+    }
+
+    class ChannelItem
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string tvid { get; set; }
+
+    }
+
+
+    class ChannelPort
+    {
+        public List<ChannelPortItem> chitem;
+        public ChannelPort()
+        {
+            chitem = new List<ChannelPortItem>();
+        chitem.Add(new ChannelPortItem { ChannelId="209", ServiceId="134100_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-209.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="109",ServiceId="134100_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-109.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="210",ServiceId="134110_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-210.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="110",ServiceId="134110_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-110.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="211",ServiceId="101400_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-211.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="111",ServiceId="101400_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-111.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="205",ServiceId="134160_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-205.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="105",ServiceId="134160_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-105.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="202",ServiceId="134230_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-202.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="102",ServiceId="134230_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-102.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="201",ServiceId="134210_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-201.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="101",ServiceId="134210_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-101.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="204",ServiceId="134200_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-204.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="104",ServiceId="134200_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-104.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="203",ServiceId="134220_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-203.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="103",ServiceId="134220_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-103.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="212",ServiceId="134350_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-212.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="112",ServiceId="134350_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-112.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="213",ServiceId="112230_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-213.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="113",ServiceId="112230_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-113.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="214",ServiceId="134130_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-214.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="114",ServiceId="134130_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-114.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="215",ServiceId="134340_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-215.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="115",ServiceId="134340_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-115.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="216",ServiceId="134310_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-216.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="116",ServiceId="134310_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-116.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="217",ServiceId="134320_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-217.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="117",ServiceId="134320_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-117.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="218",ServiceId="134330_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-218.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="118",ServiceId="134330_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-118.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="414",ServiceId="112400_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-414.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="28", ServiceId="112400_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-28.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="413", ServiceId="101490_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-413.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="27", ServiceId="101490_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-27.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="443", ServiceId="112270_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-443.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="8", ServiceId="112270_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-8.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="478", ServiceId="112210_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-478.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="67", ServiceId="112210_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-67.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="475", ServiceId="112320_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-475.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="68", ServiceId="112320_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-68.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="476", ServiceId="145730_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-476.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="69", ServiceId="145730_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-69.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="474", ServiceId="112240_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-474.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="70", ServiceId="112240_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-70.m3u8"});
+        //chitem.Add(new ChannelPortItem { ChannelId="479", ServiceId="145270_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-479.m3u8"});
+        //chitem.Add(new ChannelPortItem { ChannelId="71", ServiceId="145270_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-71.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="472", ServiceId="123320_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-472.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="72", ServiceId="123320_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-72.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="417", ServiceId="123742_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-417.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="73", ServiceId="123742_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-73.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="416", ServiceId="123732_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-416.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="74", ServiceId="123732_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-74.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="418", ServiceId="101710_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-418.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="75", ServiceId="101710_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-75.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="419", ServiceId="101620_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-419.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="76", ServiceId="101620_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-76.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="480", ServiceId="123735_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-480.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="77", ServiceId="123735_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-77.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="402", ServiceId="123739_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-402.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="17", ServiceId="123739_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-17.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="481", ServiceId="101520_400k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-481.m3u8"});
+        chitem.Add(new ChannelPortItem { ChannelId="482", ServiceId="101520_1000k",URL="http://scgd-m3u8.joyseetv.com:8080/trans_m3u8/DWHLS-482.m3u8"});
+
+
+        }
+
+
+    }
+
+    class ChannelPortItem
+    {
+        public string ChannelId { get; set; }
+        public string ServiceId { get; set; }
+        public string URL { get; set; }
+
+    }
+}
---
+++ ITVM3U8Monitor/MainWindow.xaml.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ITVM3U8Monitor
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int powerState = 0;
+        ClassChannelName channelname = new ClassChannelName();
+        ChannelPort channelport = new ChannelPort();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string ss=Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            MainWindow1.Title += "   成都爱家TV   " + ss;
+
+        }
+
+        private async void ButtonPower_Click(object sender, RoutedEventArgs e)
+        {
+
+                
+                resultsTextBox.Clear();
+                await SumPageSizesAsync();
+                resultsTextBox.Text += "\r\n下载完成.\r\n";
+
+
+        }
+
+        private async Task SumPageSizesAsync()
+        {
+            // Make a list of web addresses.
+            List<string> urlList = SetUpURLList();
+
+            // Create a query. 
+            IEnumerable<Task<int>> downloadTasksQuery =
+                from url in urlList select ProcessURLAsync(url);
+
+            // Use ToArray to execute the query and start the download tasks.
+            Task<int>[] downloadTasks = downloadTasksQuery.ToArray();
+
+            // You can do other work here before awaiting.
+
+            // Await the completion of all the running tasks.
+            int[] lengths = await Task.WhenAll(downloadTasks);
+
+            //// The previous line is equivalent to the following two statements.
+            //Task<int[]> whenAllTask = Task.WhenAll(downloadTasks);
+            //int[] lengths = await whenAllTask;
+
+            int total = lengths.Sum();
+
+
+            // Display the total count for all of the websites.
+            resultsTextBox.Text +=
+                string.Format("\r\n\r\nTotal bytes returned:  {0}\r\n", total);
+        }
+
+        private List<string> SetUpURLList()
+        {
+            List<string> urls = new List<string>();
+            string key = "";
+            if (textboxKey.Text != "")
+            {
+                key = "&key=" + textboxKey.Text;
+
+
+                foreach (var val in channelname.chitem)
+                {
+                    urls.Add("http://218.200.229.165:10009/m3u8/m3u8?id=" + val.tvid + "&version=1&rate=400k" + key);
+                    urls.Add("http://218.200.229.165:10009/m3u8/m3u8?id=" + val.tvid + "&version=1&rate=1000k" + key);
+                }
+            }
+            else
+            {
+                Random rm = new Random();
+                foreach(var val in channelport.chitem)
+                {
+                    
+                    urls.Add(val.URL+"?v="+rm.Next(2000));
+                }
+
+            }
+            return urls;
+        }
+
+
+
+        // The actions from the foreach loop are moved to this async method.
+        private async Task<int> ProcessURLAsync(string url)
+        {
+            var byteArray = await GetURLContentsAsync(url);
+            DisplayResults(url, byteArray);
+            return byteArray.Length;
+        }
+
+
+        private async Task<byte[]> GetURLContentsAsync(string url)
+        {
+            // The downloaded resource ends up in the variable named content.
+            var content = new MemoryStream();
+
+            // Initialize an HttpWebRequest for the current URL.
+            var webReq = (HttpWebRequest)WebRequest.Create(url);
+
+            //webReq.UserAgent = "AppleCoreMedia/1.0.0.12B436 (iPhone; U; CPU OS 8_1_1 like Mac OS X; zh_cn)";
+            // Send the request to the Internet resource and wait for
+            // the response.
+            using (WebResponse response = await webReq.GetResponseAsync())
+            {
+                // Get the data stream that is associated with the specified url.
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    await responseStream.CopyToAsync(content);
+                }
+            }
+
+            // Return the result as a byte array.
+            return content.ToArray();
+
+        }
+
+        public static DateTime ConverToTime(int time)
+        {
+            DateTime time197011 = new DateTime(1970, 1, 1);
+            time197011 = time197011.AddSeconds(time);
+            TimeZone localZone = TimeZone.CurrentTimeZone;
+            TimeSpan ts = localZone.GetUtcOffset(time197011);
+            time197011 += ts;
+
+            return time197011;
+        }
+        private void DisplayResults(string url, byte[] content)
+        {
+            // Display the length of each website. The string format 
+            // is designed to be used with a monospaced font, such as
+            // Lucida Console or Global Monospace.
+            var bytes = content.Length;
+            if (bytes == 0)
+                return;
+            string strcontent = System.Text.Encoding.UTF8.GetString(content);
+            //string strcontent = BitConverter.ToString(content);
+            var lines = strcontent.Split(new char[] { '\n' });
+
+
+            if (textboxKey.Text != "")
+            {
+                int i = lines.Count();
+                var lastline = lines[i - 2];
+                var streamtime = lastline.Split(new char[] { '_' })[2].Split(new char[] { '.' })[0];
+                DateTime dt = ConverToTime(int.Parse(streamtime));
+                string status = "";
+                if (DateTime.Now.Subtract(dt).TotalMinutes > 10)
+                {
+                    status = "   错误！";
+                }
+                // Strip off the "http://".
+                var displayURL = url.Replace("http://", "");
+                var para = displayURL.Split(new char[] { '?' })[1];
+                var id = para.Split(new char[] { '&' })[0].Split(new char[] { '=' })[1];
+                var rate = para.Split(new char[] { '&' })[2].Split(new char[] { '=' })[1];
+
+                var name = "";
+                foreach (var val in channelname.chitem)
+                {
+                    if (val.tvid == id)
+                        name = val.name;
+                }
+
+
+                resultsTextBox.Text += string.Format("{0} {1,-8} {2,-8} {3,-8} {4,-16} \n ", name.PadRight(30, ' '), id, rate, bytes + "bytes", dt.ToString() + status);
+            }
+            else
+            {
+                int i = lines.Count();
+                var lastline = lines[i - 1];
+                var streamtime = lastline.Split(new char[] { '_' })[2].Split(new char[] { '.' })[0];
+                DateTime dt = ConverToTime(int.Parse(streamtime));
+                string status = "";
+                if (DateTime.Now.Subtract(dt).TotalMinutes > 10)
+                {
+                    status = "   错误！";
+                }
+                var displayURL = url.Replace("http://", "");
+                var para = displayURL.Split(new char[] { '/' })[2];
+                var id = para.Split(new char[] { '.' })[0].Split(new char[] { '-' })[1];
+
+                var name = "";
+                foreach (var val in channelport.chitem)
+                {
+                    if (val.ChannelId == id)
+                        name = val.ServiceId;
+                }
+
+                resultsTextBox.Text += string.Format("{0} {1,-8} {2,-8} {3,-16} \n ", name.PadRight(30, ' '), id, bytes + "bytes", dt.ToString() + status);
+            }
+
+
+        }
+
+
+    }
+}
